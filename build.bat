47cec182a02a3fd7af8281582ab4a35d8aeb3e40
Dyllan Cole <dyllancole@outlook.com>
2018-10-20 16:56:48 -0400

    idk

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-bcc -ml -2 -Isrc\ src\noctis.cpp src\noctis-0.cpp src\noctis-1.cpp
-copy /b src\noctis.exe+res\supports.nct=src\noctis.exe
+bcc -ml -4 -Isrc\ src\noctis.cpp src\noctis-0.cpp src\noctis-1.cpp
+copy /b noctis.exe+res\supports.nct=noctis.exe
