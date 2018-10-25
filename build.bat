14ed74721ad6de067ae589427f5e17fa2c0c10f7
Dyllan Cole <dyllancole@outlook.com>
2018-10-25 10:53:57 -0400

    Automatic far pointers

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-bcc -ml -4 -Isrc\ src\noctis.cpp src\noctis-0.cpp src\noctis-1.cpp
+bcc -ml -4 -Ff -Isrc\ src\noctis.cpp src\noctis-0.cpp src\noctis-1.cpp
 copy /b noctis.exe+res\supports.nct=noctis.exe
 del noctis-0.obj noctis-1.obj noctis.obj
