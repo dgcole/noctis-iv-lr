178f39051cf3eb2dc159c3ee4aeea90ae9c602ce
Dyllan Cole <dyllancole@outlook.com>
2018-10-28 14:30:57 -0400

    Update build.bat to include debug info

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-bcc -ml -4 -Isrc\ src\noctis.cpp src\noctis-0.cpp src\noctis-1.cpp
+bcc -ml -4 -v -Isrc\ src\noctis.cpp src\noctis-0.cpp src\noctis-1.cpp
 del noctis-0.obj noctis-1.obj noctis.obj
