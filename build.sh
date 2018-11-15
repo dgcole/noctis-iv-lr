927efbe31f9400bd99d932bdbafcab33e61996c0
Dyllan Cole <dyllancole@outlook.com>
2018-11-14 19:05:30 -0500

    Cleanup

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wine bcc -mh -4 -ISRC/ SRC/NOCTIS.CPP SRC/NOCTIS-0.CPP SRC/NOCTIS-1.CPP
+rm *.obj
--- link.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-tlink C:\BC5\LIB\c0l.obj+src\noctis+src\noctis-0+src\noctis-1,noctis,/c/x,C:\BC5\LIB\EMU.LIB+C:\BC5\LIB\mathl.lib+C:\BC5\LIB\cl.lib
-del src\noctis.obj
-del src\noctis-0.obj
-del src\noctis-1.obj
